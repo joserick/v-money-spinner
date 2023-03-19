4e793d5b0882a267416938c4d44ca8e3023e73b0
joserick <support@joserick.com>
2023-03-19 03:32:00 -0600

    chore: Added plugin for dts generator

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -16,7 +17,12 @@ export default defineConfig(({ mode }) => {
   }
 
   return {
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      dts({
+        insertTypesEntry: true,
+      }),
+    ],
     build: {
       target: 'es2020',
       lib: {
